75f99741aec6da3e765912e38f0523b325ff852e
sadeghjafari5528 <sadeghjafari5528@gmail.com>
2022-05-22 16:30:04 +0430

    start Statistics funcitons

---
+++ src/statistics.py
@@ -0,0 +1,21 @@
+
+
+
+class Statistics:
+    def __init__(self, data):
+        self.data = data
+
+    def get_no_data(self):
+        pass
+
+    def get_no_sentence(self):
+        pass
+
+    def get_no_words(self):
+        pass
+
+    def get_no_types(self):
+        pass
+
+    def get_histogram(self):
+        pass
